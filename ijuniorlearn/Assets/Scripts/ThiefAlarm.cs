e4aebacd167f10a1ccc5fe1db91b8bc55c117aae
vanady1 <88271436+vanady1@users.noreply.github.com>
2022-03-04 17:18:47 +0100

    add project

---
+++ ijuniorlearn/Assets/Scripts/ThiefAlarm.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class ThiefAlarm : MonoBehaviour
+{
+    [SerializeField] private Tilemap _tilemap;
+    [SerializeField] private Color _defaultColor;
+    [SerializeField] private AudioSource _alarmEffect;
+    [SerializeField] private float _timePassed;
+
+    private void Start()
+    {
+        _tilemap = gameObject.GetComponent<Tilemap>();
+        _defaultColor = _tilemap.color;
+        _alarmEffect = gameObject.GetComponent<AudioSource>();
+        _alarmEffect.Stop();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent<PhysicsMovement>(out PhysicsMovement thief))
+        {
+            _tilemap.color = Color.red;
+            _alarmEffect.Play();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent<PhysicsMovement>(out PhysicsMovement thief))
+        {
+            _tilemap.color = _defaultColor;
+            _alarmEffect.Stop();
+        }
+    }
+    private void Update()
+    {
+        if (_alarmEffect.isPlaying)
+        {
+            _timePassed += Time.deltaTime;
+
+            _alarmEffect.volume = Mathf.Sin(_timePassed) * 0.5f + 0.5f;
+        }
+    }
+}
