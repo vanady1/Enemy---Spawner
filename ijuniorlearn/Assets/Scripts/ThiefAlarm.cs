33606ff2f59915fde8dde84fccb5f216ecdc4fc4
vanady1 <88271436+vanady1@users.noreply.github.com>
2022-03-05 14:37:56 +0100

    Add changes

--- ijuniorlearn/Assets/Scripts/ThiefAlarm.cs
+++ ijuniorlearn/Assets/Scripts/ThiefAlarm.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
+
 public class ThiefAlarm : MonoBehaviour
 {
     [SerializeField] private Tilemap _tilemap;
     [SerializeField] private Color _defaultColor;
     [SerializeField] private AudioSource _alarmEffect;
-    [SerializeField] private float _timePassed;
+    [SerializeField] private float _soundChangingDuration;
+    [SerializeField] private float _minVolume;
+    [SerializeField] private float _maxVolume;
 
     private void Start()
     {
@@ -16,6 +19,7 @@ public class ThiefAlarm : MonoBehaviour
         _defaultColor = _tilemap.color;
         _alarmEffect = gameObject.GetComponent<AudioSource>();
         _alarmEffect.Stop();
+        _alarmEffect.volume = _minVolume;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -24,6 +28,8 @@ public class ThiefAlarm : MonoBehaviour
         {
             _tilemap.color = Color.red;
             _alarmEffect.Play();
+            StopAllCoroutines();
+            StartCoroutine(IncreaseVolume());
         }
     }
 
@@ -32,16 +38,29 @@ public class ThiefAlarm : MonoBehaviour
         if (collision.TryGetComponent<PhysicsMovement>(out PhysicsMovement thief))
         {
             _tilemap.color = _defaultColor;
-            _alarmEffect.Stop();
+            StopAllCoroutines();
+            StartCoroutine(DecreaseVolume());
         }
     }
-    private void Update()
-    {
-        if (_alarmEffect.isPlaying)
-        {
-            _timePassed += Time.deltaTime;
 
-            _alarmEffect.volume = Mathf.Sin(_timePassed) * 0.5f + 0.5f;
+    private IEnumerator IncreaseVolume()
+    {
+        while (_alarmEffect.volume < 1)
+        {
+            _alarmEffect.volume += Time.deltaTime / _soundChangingDuration;
+
+            yield return new WaitForSeconds(0.01f);
         }
+    }
+    private IEnumerator DecreaseVolume()
+    {
+        while (_alarmEffect.volume > 0)
+        {
+            
+            _alarmEffect.volume -= Time.deltaTime / _soundChangingDuration;
+
+            yield return new WaitForSeconds(0.01f);
+        }
+        _alarmEffect.Stop();
     }
 }
