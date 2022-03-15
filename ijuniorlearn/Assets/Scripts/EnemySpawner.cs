74a4e1831a4cf1e3af624b5899603debbb2b49d8
vanady1 <88271436+vanady1@users.noreply.github.com>
2022-03-15 19:55:50 +0100

    add changes

---
+++ ijuniorlearn/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject _enemyTemplate;
+    [SerializeField] private float _timeBetweenSpawns;
+    private Transform _spawner;
+    private Transform[] _spawningPoints;
+    private int _currentPoint;
+
+    private void Start()
+    {
+        _spawner = GetComponent<Transform>();
+        _spawningPoints = new Transform[_spawner.childCount];
+
+        for (int i = 0; i < _spawner.childCount; i++)
+        {
+            _spawningPoints[i] = _spawner.GetChild(i);
+        }
+
+        StartCoroutine(SpawnPeriodically(_timeBetweenSpawns));
+    }
+
+    private IEnumerator SpawnPeriodically(float timeAmount)
+    {
+        for (int i = 0; i < _spawningPoints.Length; i++)
+        {
+            Instantiate(_enemyTemplate, _spawningPoints[i]);
+            yield return new WaitForSeconds(timeAmount);
+        }
+    }
+}
