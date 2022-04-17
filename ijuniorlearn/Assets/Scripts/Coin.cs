0de77ea6bb88206fdeee6ff34b2a2327e3a85b17
vanady1 <88271436+vanady1@users.noreply.github.com>
2022-04-17 13:46:59 +0200

    Simple2DPlatformer

---
+++ ijuniorlearn/Assets/Scripts/Coin.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class Coin : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent<PhysicsMovement>(out PhysicsMovement player))
+        {
+            Gather();
+        }
+    }
+
+    private void Gather()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ ijuniorlearn/Assets/Scripts/PhysicsMovement.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Animator))]
+public class PhysicsMovement : MonoBehaviour
+{
+    [SerializeField] private float _minGroundNormalY = .65f;
+    [SerializeField] private float _gravityModifier = 1f;
+    [SerializeField] private Vector2 _velocity;
+    [SerializeField] private LayerMask _layerMask;
+    [SerializeField] private bool _grounded;
+
+    private bool _isFlipped;
+    private Animator _animator;
+    private Vector2 _targetVelocity;
+    private Vector2 _groundNormal;
+    private Rigidbody2D _rigidbody2D;
+    private ContactFilter2D _contactFilter;
+    private RaycastHit2D[] _hitBuffer = new RaycastHit2D[16];
+    private List<RaycastHit2D> _hitBufferList = new List<RaycastHit2D>(16);
+
+    private const float _minMoveDistance = 0.001f;
+    private const float _shellRadius = 0.01f;
+
+    private void OnEnable()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        _contactFilter.useTriggers = false;
+        _contactFilter.SetLayerMask(_layerMask);
+        _contactFilter.useLayerMask = true;
+        _animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        _targetVelocity = new Vector2(Input.GetAxis("Horizontal"), 0);
+
+        if (Input.GetKey(KeyCode.Space) && _grounded)
+        {
+            _velocity.y = 5;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        _velocity += _gravityModifier * Physics2D.gravity * Time.deltaTime;
+        _velocity.x = _targetVelocity.x;
+
+        if (_velocity.x > 0 && !_isFlipped)
+        {
+            Flip();
+        }
+        else if(_velocity.x < 0 && _isFlipped)
+        {
+            Flip();
+        }
+        _animator.SetFloat("Velocity", Mathf.Abs(_velocity.x) );
+
+        _grounded = false;
+
+        _animator.SetBool("Grounded", false);
+
+        Vector2 deltaPosition = _velocity * Time.deltaTime;
+        Vector2 moveAlongGround = new Vector2(_groundNormal.y, -_groundNormal.x);
+        Vector2 move = moveAlongGround * deltaPosition.x;
+
+        Movement(move, false);
+
+        move = Vector2.up * deltaPosition.y;
+
+        Movement(move, true);
+    }
+
+    private void Flip()
+    {
+        Vector3 currentScale = gameObject.transform.localScale;
+        currentScale.x *= -1;
+        gameObject.transform.localScale = currentScale;
+
+        _isFlipped = !_isFlipped;
+    }
+
+    private void Movement(Vector2 move, bool yMovement)
+    {
+        float distance = move.magnitude;
+
+        if (distance > _minMoveDistance)
+        {
+            int count = _rigidbody2D.Cast(move, _contactFilter, _hitBuffer, distance + _shellRadius);
+
+            _hitBufferList.Clear();
+
+            for (int i = 0; i < count; i++)
+            {
+                _hitBufferList.Add(_hitBuffer[i]);
+            }
+
+            for (int i = 0; i < _hitBufferList.Count; i++)
+            {
+                Vector2 currentNormal = _hitBufferList[i].normal;
+                if (currentNormal.y > _minGroundNormalY)
+                {
+                    _grounded = true;
+                    _animator.SetBool("Grounded", true);
+                    if (yMovement)
+                    {
+                        _groundNormal = currentNormal;
+                        currentNormal.x = 0;
+                    }
+                }
+
+                float projection = Vector2.Dot(_velocity, currentNormal);
+                if (projection < 0)
+                {
+                    _velocity = _velocity - projection * currentNormal;
+                }
+
+                float modifiedDistance = _hitBufferList[i].distance - _shellRadius;
+                distance = modifiedDistance < distance ? modifiedDistance : distance;
+            }
+        }
+
+        _rigidbody2D.position = _rigidbody2D.position + move.normalized * distance;
+    }
+}
